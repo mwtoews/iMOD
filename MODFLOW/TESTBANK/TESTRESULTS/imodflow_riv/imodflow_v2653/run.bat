c9f9c760636f28b27c6dbafb8199651bab79f985
verkaik_jo <verkaik_jo@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2014-09-25 14:01:26 +0000

    MODFLOW: v3.00.01: clean up tests.
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@52 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- MODFLOW/TESTBANK/TESTRESULTS/imodflow_riv/imodflow_v2653/run.bat
+++
@@ -1,6 +0,0 @@
-REM clean file
-IF EXIST p:\1206764-imod\imod30\testbank\output\runs\test_ALL\imodflow_riv\imodflow_v2653\ERROR.TXT DEL p:\1206764-imod\imod30\testbank\output\runs\test_ALL\imodflow_riv\imodflow_v2653\ERROR.TXT
-REM execute program
-p:\1206764-imod\imod30\ontwikkeling\imodflow\_exe\iMODFLOW_X64_V2653_VMETASWAP7220.exe p:\1206764-imod\imod30\testbank\output\runs\test_ALL\imodflow_riv\imodflow_v2653\imodflow.run
-REM echo exitcode
-IF ERRORLEVEL == 1 ECHO %ERRORLEVEL% > p:\1206764-imod\imod30\testbank\output\runs\test_ALL\imodflow_riv\imodflow_v2653\error.txt
