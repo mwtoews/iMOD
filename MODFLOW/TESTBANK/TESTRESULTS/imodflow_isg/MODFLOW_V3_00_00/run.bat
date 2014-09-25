95cea899f0fed7321e07aa2b97d8397e0d321d21
verkaik_jo <verkaik_jo@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2014-09-25 14:19:11 +0000

    MODFLOW: v3.00.01: clean up tests.
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@55 8a8853d3-af7d-47ec-b359-4bd2096ba547

---
+++ MODFLOW/TESTBANK/TESTRESULTS/imodflow_isg/MODFLOW_V3_00_00/run.bat
@@ -0,0 +1,6 @@
+REM clean file
+IF EXIST p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\MODFLOW_V3_00_00\ERROR.TXT DEL p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\MODFLOW_V3_00_00\ERROR.TXT
+REM execute program
+p:\1206764-imod\imod30\ontwikkeling\imodflow\_exe\MODFLOW_X64R_V3_0.exe p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\MODFLOW_V3_00_00\imodflow.run
+REM echo exitcode
+IF ERRORLEVEL == 1 ECHO %ERRORLEVEL% > p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\MODFLOW_V3_00_00\error.txt
