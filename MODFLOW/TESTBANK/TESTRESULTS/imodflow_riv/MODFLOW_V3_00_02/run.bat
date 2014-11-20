66a6261fac9064c6a453007f174aebdd7dd9fdd3
verkaik_jo <verkaik_jo@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2014-11-20 08:59:12 +0000

    MODFLOW: v3.00.02 test results added.
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@83 8a8853d3-af7d-47ec-b359-4bd2096ba547

---
+++ MODFLOW/TESTBANK/TESTRESULTS/imodflow_riv/MODFLOW_V3_00_02/run.bat
@@ -0,0 +1,6 @@
+REM clean file
+IF EXIST p:\1206764-imod\imod30\testbank\output_V3_00_02\runs\test_ALL\imodflow_riv\MODFLOW_V3_00_02\ERROR.TXT DEL p:\1206764-imod\imod30\testbank\output_V3_00_02\runs\test_ALL\imodflow_riv\MODFLOW_V3_00_02\ERROR.TXT
+REM execute program
+p:\1206764-imod\imod30\ontwikkeling\imodflow\_exe\MODFLOW_V3_00_02_METASWAP_SVN1004_X64R.exe p:\1206764-imod\imod30\testbank\output_V3_00_02\runs\test_ALL\imodflow_riv\MODFLOW_V3_00_02\imodflow.run
+REM echo exitcode
+IF ERRORLEVEL == 1 ECHO %ERRORLEVEL% > p:\1206764-imod\imod30\testbank\output_V3_00_02\runs\test_ALL\imodflow_riv\MODFLOW_V3_00_02\error.txt
