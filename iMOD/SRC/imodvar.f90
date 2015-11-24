112455a3f8d0107f66084beb81d68f41367f1f00
vermeul_pr <vermeul_pr@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2015-11-24 14:40:52 +0000

    iMOD - unstable version / developments going on
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@441 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- iMOD/SRC/imodvar.f90
+++ iMOD/SRC/imodvar.f90
@@ -26,7 +26,8 @@ INTEGER,DIMENSION(2) :: IDPROC
 
 REAL,PARAMETER :: PI=ATAN(1.0)*4.0  !##value pi
 
-CHARACTER(LEN=30),PARAMETER :: RVERSION='V3_2_0'
+CHARACTER(LEN=30),PARAMETER :: RVERSION='V3_2'    !## release message - only with single subnummers
+CHARACTER(LEN=30),PARAMETER :: BVERSION='V3_2_00' !## banner message - no characters !!!
 CHARACTER(LEN=32) :: LICFILE='I_accepted_'//TRIM(RVERSION)//'.txt'
 CHARACTER(LEN=256) :: IMFFNAME         !##name of drawing file
 CHARACTER(LEN=256) :: EXENAME,EXEPATH
