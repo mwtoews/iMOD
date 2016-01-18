990300f1ea72f278f210e51d5460cf305d653c1b
vermeul_pr <vermeul_pr@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2016-01-18 20:11:48 +0000

    iMOD - unstable version / developments going on - beta V3_3_6
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@515 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- iMOD/SRC/imodvar.f90
+++ iMOD/SRC/imodvar.f90
@@ -30,7 +30,7 @@ LOGICAL :: LBETA=.TRUE. !# if TRUE: Show question whether it is allowed to use B
 LOGICAL :: LEXPDATE=.TRUE. !# if TRUE: activate expire date
 INTEGER :: EXPDATE=20160215 !# expire data, after this date the iMOD-beta version cannot be used.
 
-CHARACTER(LEN=30),PARAMETER :: RVERSION='V3_3_5'    !## release message - only with single subnummers
+CHARACTER(LEN=30),PARAMETER :: RVERSION='V3_3_7'    !## release message - only with single subnummers
 CHARACTER(LEN=30),PARAMETER :: BVERSION='Beta'      !## banner message !!!
 CHARACTER(LEN=32) :: LICFILE='I_accepted_'//TRIM(RVERSION)//'.txt'
 CHARACTER(LEN=256) :: IMFFNAME         !## name of drawing file
