4e8a39cc9d17d4d9da66eabff04ee33dab1d2403
burgerin <burgerin@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2016-01-22 09:57:44 +0000

    Liduin - unstable version, under construction - flumy
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@525 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- iMOD/SRC/imod_batch/imod_batch_flumy.f90
+++ iMOD/SRC/imod_batch/imod_batch_flumy.f90
@@ -29,6 +29,8 @@ USE MOD_IPF_PAR
 USE MOD_IPF, ONLY : IPFREAD2,IPFALLOCATE,IPFDEALLOCATE
 USE MOD_IPFASSFILE,ONLY : IPFOPENASSFILE,IPFREADASSFILELABEL,IPFREADASSFILE,IPFASSFILEALLOCATE
 USE MOD_TSTAT_PAR
+USE MOD_OSD, ONLY : OSD_OPEN
+USE MOD_UTL, ONLY : UTL_CAP,UTL_GETUNIT
 
 CONTAINS
 
@@ -36,9 +38,12 @@ CONTAINS
  SUBROUTINE FLUMY_MAIN()
  !###======================================================================
  IMPLICIT NONE
- INTEGER :: IU,I,J,K,X,Y,NIPF,ACOL
+ INTEGER :: IU,JU,I,J,K,M,X,Y,NIPF,ACOL,IOS
  CHARACTER(LEN=256) :: FNAME,IPFNAME,DIR
  REAL :: ZTOP,ZBOT
+ CHARACTER(LEN=12),DIMENSION(:),ALLOCATABLE :: GRAIN
+ CHARACTER(LEN=2),DIMENSION(:),ALLOCATABLE :: FACL
+ INTEGER,DIMENSION(:),ALLOCATABLE :: FACN
  
  NIPF=1; CALL IPFALLOCATE()
  IPF(1)%XCOL =1 !## x
@@ -64,29 +69,68 @@ CONTAINS
   J=INDEX(IPF(1)%FNAME,'\',.TRUE.)
   DIR=IPF(1)%FNAME(1:J-1)
 
-  FNAME=TRIM(DIR)//'\'//TRIM(IPF(1)%INFO(ACOL,K))//'.'//TRIM(ADJUSTL(IPF(1)%FEXT))
+  FNAME=TRIM(DIR)//'\'//TRIM(IPF(1)%INFO(ACOL,I))//'.'//TRIM(ADJUSTL(IPF(1)%FEXT))
   !## read dimensions of associated file
   IF(IPFOPENASSFILE(IU,1,FNAME))THEN
-   !## drill found
+   !# allocate needed variables
+   ALLOCATE(GRAIN(ASSF(1)%NRASS)); ALLOCATE(FACL(ASSF(1)%NRASS)); ALLOCATE(FACN(ASSF(1)%NRASS))
+   !## drill found   
    IF(ASSF(1)%ITOPIC.EQ.2)THEN
     IF(IPFREADASSFILELABEL(IU,1,FNAME).AND.  &
      IPFREADASSFILE(IU,1,FNAME))THEN
      ZTOP=ASSF(1)%Z(1) !# top level of borehole
      ZBOT=ASSF(1)%Z(ASSF(1)%NRASS) !# bot level of borehole 
+     
+     !#MOET NOG ANDERS, OP DEZE MANIER KAN HET NIET...
+     !#search for lithology class in data string, and compare with GRAIN-variable in batch-file.
      DO K=1,ASSF(1)%NRASS !loop over rows associated file
-      !ASSF(1)%L(:,K) !#search for lithology class in data string, and compare with GRAIN-variable in batch-file.
-      !# make type (in iMOD-batch) which contains the grain/faciesl/faciesn parameters, zodat de betreffende kolom direct gevuld kan worden met de juiste waarden gekoppeld aan de gevonden lithologie.
+      DO M=1,NPAR
+!       IF(TRIM(UTL_CAP(ASSF(1)%L(4,K),'U')).EQ.TRIM(UTL_CAP(FLM(1)%GRN(M)),'U'))THEN
+!        FACN(K)=FLM(1)%FCN(M)
+!        FACL(K)=FLM(1)%FCL(M)
+!        GRAIN(K)=FLM(1)%GRN(M)
+!       ENDIF
+      ENDDO
      ENDDO
+    
     ENDIF
    ENDIF
   ENDIF
      
-   !## WEGSCHRIJVEN NAAR TXT FILE
-!     IPF(1)%INFO(:,J)=IPF(1)%INFO(:,I)
-
+  !## write data to txt-file in flumy-format
+  JU=UTL_GETUNIT()
+  FNAME=TRIM(DIR)//'\'//TRIM(IPF(1)%INFO(ACOL,I))//'.txt'
+  CALL OSD_OPEN(JU,FILE=FNAME,STATUS="UNKNOWN",ACTION='WRITE',IOSTAT=IOS)
+  WRITE(JU,*) "# ====================================================================="
+  WRITE(JU,*) "# Well "//TRIM(IPF(1)%INFO(ACOL,I))
+  WRITE(JU,*) "#"
+  WRITE(JU,*) "# Coordinates, depth and thickness are expressed in meters"
+  WRITE(JU,*) "# ====================================================================="
+  WRITE(JU,*) "# Well Location"
+  WRITE(JU,*) "X_WELL=",X
+  WRITE(JU,*) "Y_WELL=",Y
+  WRITE(JU,*) "#"
+  WRITE(JU,*) "# Bottom elevation"
+  WRITE(JU,*) "Z_BOTTOM=",ZBOT
+  WRITE(JU,*) "# Top elevation"
+  WRITE(JU,*) "Z_TOP=",ZTOP
+  WRITE(JU,*) "#"
+  WRITE(JU,*) "# Deposits From top to bottom"
+  WRITE(JU,*) "# Facies_id Facies Depth"
+  WRITE(JU,*) "#   Warning : Depth from top of deposit basis"
+  WRITE(JU,*) "ATTRIBUTE_COLUMN=1"
+  WRITE(JU,*) "DEPTH_COLUMN=3"
+  WRITE(JU,*) "DISCRETE_ATTRIBUTE=1"
+  WRITE(JU,*) "STANDARD_FACIES=1"
+  WRITE(JU,*) "~Ascii"
+  DO K=1,ASSF(1)%NRASS
+   !WRITE(JU,*) FACN(K),TRIM(UTL_CAP(FACL(K),'U')),ASSF(1)%Z(K)
+  ENDDO
+  CLOSE(JU)
  ENDDO
-   
-  CALL IPFDEALLOCATE()
+ 
+ DEALLOCATE(GRAIN); DEALLOCATE(FACL); DEALLOCATE(FACN)   
+ CALL IPFDEALLOCATE()
  
  END SUBROUTINE FLUMY_MAIN
 
