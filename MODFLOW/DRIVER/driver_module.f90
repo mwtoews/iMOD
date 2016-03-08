58680d4b7495db030a211e7118e012286030eeed
verkaik_jo <verkaik_jo@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2016-03-08 12:34:50 +0000

    Update iMOD license v3_3.
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@592 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- MODFLOW/DRIVER/driver_module.f90
+++ MODFLOW/DRIVER/driver_module.f90
@@ -1156,7 +1156,7 @@ use imod_utl, only: imod_utl_getunit, imod_utl_getdir, imod_utl_s_cap,&
 implicit none
 
 ! parameters
-character(len=1024), parameter :: licfile = 'I_accepted_v3_2.txt'
+character(len=1024), parameter :: licfile = 'I_accepted_v3_3.txt'
 integer, parameter :: nlic = 33
 character(len=79), dimension(nlic) :: lic
 integer, parameter :: nhdr = 39
@@ -1164,7 +1164,7 @@ character(len=79), dimension(nhdr) :: hdr
 
 !         1234567890123456789012345678901234567890123456789012345678901234567890123456789
 data hdr/'===============================================================================',&!01
-         'iMODFLOW Version 3_2_1, November 2015                                          ',&!02
+         'iMODFLOW Version 3_3, March 2016                                               ',&!02
          '                                                                               ',&!03
          'Copyright (C) Stichting Deltares, 2005-2015.                                   ',&!04
          '                                                                               ',&!05
@@ -1179,7 +1179,7 @@ data hdr/'======================================================================
          'Please go to the PDF-file of the iMOD License, read it and decide whether you  ',&!04 
          'want or do not want to accept the iMOD License.                                ',&!05 
          '                                                                               ',&!06
-         'According to the file "I_accepted_v3_2.txt" on your computer you accepted the  ',&!07
+         'According to the file "I_accepted_v3_3.txt" on your computer you accepted the  ',&!07
          'terms and conditions of the iMOD license; WARNING: IF IT WAS NOT YOU OR THE    ',&!08
          'LEGAL ENTITY ON WHOSE BEHALF YOU INTENT TO USE THE IMOD-EXECUTABLE, THAT       ',&!09 
          'ACCEPTED THE TERMS AND CONDITIONS OF THE iMOD LICENSE YOU ARE NOT ENTITLED TO  ',&!10
@@ -1191,7 +1191,7 @@ data hdr/'======================================================================
          'accepted by the legal entity on whose behalf you intent to use the             ',&!06 
          'iMOD-executable byre-invoking the ‘I accept’-procedure; to re-invoke the       ',&!07
          '"I accept"-procedure abort the use of this Deltares-executable of iMOD, delete ',&!08
-         'the file "I_accepted_v3_2.txt", and invoke this Deltares-executable of iMOD    ',&!09
+         'the file "I_accepted_v3_3.txt", and invoke this Deltares-executable of iMOD    ',&!09
          'again.                                                                         ',&!10
          '                                                                               ',&!01
          'The iMOD software is distributed in the hope that it will be useful, but       ',&!02
