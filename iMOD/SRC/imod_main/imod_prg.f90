0dca2effa4449d153c7ae04b0a0389b83a9007be
vermeul_pr <vermeul_pr@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2016-03-18 19:08:15 +0000

    iMOD - unstable version / developments going on
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@608 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- iMOD/SRC/imod_main/imod_prg.f90
+++ iMOD/SRC/imod_main/imod_prg.f90
@@ -217,6 +217,8 @@ ZM%ZOOMXY(ZM%NZOOM,4)=MPW%YMAX
 CALL DEMO_MAIN()  !## calls demo-version iMOD directly after initialisation of iMOD manager and plotting available files
 IF(DEMO%ISAVEBMP.EQ.1)THEN; CALL WINDOWCLOSE(); STOP; ENDIF
 
+IF(LBETA)CALL WMENUSETSTATE(ID_GEOCONNECT,1,1)
+
 !## initiate timers
 CALL MAIN_TIMERS()
 
--- iMOD/SRC/imodvar.f90
+++ iMOD/SRC/imodvar.f90
@@ -26,11 +26,11 @@ INTEGER,DIMENSION(2) :: IDPROC
 
 REAL,PARAMETER :: PI=ATAN(1.0)*4.0  !## value pi
 
-LOGICAL :: LBETA=.FALSE.      !## if TRUE: Show question whether it is allowed to use Beta-version or not
+LOGICAL :: LBETA=.TRUE.      !## if TRUE: Show question whether it is allowed to use Beta-version or not
 LOGICAL :: LEXPDATE=.TRUE.   !## if TRUE: activate expire date
-INTEGER :: EXPDATE=20160331  !## expire data, after this date the iMOD-beta version cannot be used.
+INTEGER :: EXPDATE=20160415  !## expire data, after this date the iMOD-beta version cannot be used.
 
-CHARACTER(LEN=30),PARAMETER :: RVERSION='V3_3_15'    !## release message - only with single subnummers
+CHARACTER(LEN=30),PARAMETER :: RVERSION='V3_3_16'    !## release message - only with single subnummers
 CHARACTER(LEN=30),PARAMETER :: BVERSION='Beta'       !## banner message !!!
 CHARACTER(LEN=32) :: LICFILE='I_accepted_'//TRIM(RVERSION)//'.txt'
 CHARACTER(LEN=256) :: IMFFNAME         !## name of drawing file
