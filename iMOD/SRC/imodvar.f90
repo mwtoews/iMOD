505612b830ce91d07484aaac1fc8b363cf3f7ebe
vermeul_pr <vermeul_pr@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2016-05-18 15:34:09 +0000

    iMOD - unstable version / developments going on
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@734 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- iMOD/SRC/imodvar.f90
+++ iMOD/SRC/imodvar.f90
@@ -28,7 +28,7 @@ REAL,PARAMETER :: PI=ATAN(1.0)*4.0  !## value pi
 
 LOGICAL :: LBETA=.TRUE.        !## if TRUE: Show question whether it is allowed to use Beta-version or not
 LOGICAL :: LEXPDATE=.TRUE.     !## if TRUE: activate expire date
-INTEGER :: EXPDATE=20160515    !## expire data, after this date the iMOD-beta version cannot be used.
+INTEGER :: EXPDATE=20160630    !## expire data, after this date the iMOD-beta version cannot be used.
 
 CHARACTER(LEN=30),PARAMETER :: RVERSION='V3_4_5'     !## release message - only with single subnummers
 CHARACTER(LEN=30),PARAMETER :: BVERSION='Beta'       !## banner message !!!
