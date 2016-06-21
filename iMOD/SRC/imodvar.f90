10a9cacba693762339e536ea053d412dedc0468a
vermeul_pr <vermeul_pr@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2016-06-21 10:58:13 +0000

    Release candidate v3_4
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@829 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- iMOD/SRC/imodvar.f90
+++ iMOD/SRC/imodvar.f90
@@ -27,11 +27,11 @@ INTEGER,DIMENSION(2) :: IDPROC
 REAL,PARAMETER :: PI=ATAN(1.0)*4.0  !## value pi
 
 LOGICAL :: LBETA=.FALSE.       !## if TRUE: Show question whether it is allowed to use Beta-version or not
-LOGICAL :: LEXPDATE=.TRUE.     !## if TRUE: activate expire date
+LOGICAL :: LEXPDATE=.FALSE.    !## if TRUE: activate expire date
 INTEGER :: EXPDATE=20160630    !## expire data, after this date the iMOD-beta version cannot be used.
 INTEGER,SAVE :: ICDEBUGLEVEL   !## applied current debuglevel
 
-CHARACTER(LEN=30),PARAMETER :: RVERSION='V3_4_14'     !## release message - only with single subnummers
+CHARACTER(LEN=30),PARAMETER :: RVERSION='V3_4'       !## release message - only with single subnummers
 CHARACTER(LEN=30),PARAMETER :: BVERSION='Beta'       !## banner message !!!
 CHARACTER(LEN=32) :: LICFILE='I_accepted_'//TRIM(RVERSION)//'.txt'
 CHARACTER(LEN=256) :: IMFFNAME         !## name of drawing file
