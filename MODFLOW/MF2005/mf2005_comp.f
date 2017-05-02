14410ab1db8894eaf7ed771f588bdeecc9a6879d
vermeul_pr <vermeul_pr@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2017-05-02 09:18:06 +0000

    warning opnieuw aangezet
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@1332 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- MODFLOW/MF2005/mf2005_comp.f
+++ MODFLOW/MF2005/mf2005_comp.f
@@ -2011,7 +2011,7 @@ c ------------------------------------------------------------------------------
       js = 1
       do i = 1, nid
          je = xchOff(i)
-!         if (je-js.gt.0) write(*,*) 'Warning: cell received > 1 uszflux'
+         if (je-js.gt.0) write(*,*) 'Warning: cell received > 1 uszflux'
          do j = js, je
             k = xchIdx(j)
             if (k.le.0) then
@@ -2071,7 +2071,7 @@ c ------------------------------------------------------------------------------
          irow = dxcir(i)
          icol = dxcic(i)
          je = xchOff(i)
-!         if (je-js.gt.0) write(*,*) 'Warning: cell received > 1 sf1'
+         if (je-js.gt.0) write(*,*) 'Warning: cell received > 1 sf1'
          s = mv
          do j = js, je
             k = xchIdx(j)
@@ -2132,7 +2132,7 @@ c ------------------------------------------------------------------------------
          irow = dxcir(i)
          icol = dxcic(i)
          je = xchOff(i)
-!         if (je-js.gt.0) write(*,*) 'Warning: cell received > 1 sf1'
+         if (je-js.gt.0) write(*,*) 'Warning: cell received > 1 sf1'
          s = mv
          do j = js, je
             k = xchIdx(j)
@@ -2961,7 +2961,7 @@ c ------------------------------------------------------------------------------
       js = 1
       do i = 1, nid
          je = xchOff(i)
-!         if (je-js.gt.0) write(*,*) 'Warning: cell received > 1 uszflux'
+         if (je-js.gt.0) write(*,*) 'Warning: cell received > 1 uszflux'
          do j = js, je
             k = xchIdx(j)
             if (k.le.0) then
@@ -3009,9 +3009,9 @@ c ------------------------------------------------------------------------------
       js = 1
       do i = 1, nid
          je = xchOff(i)
-!         if (je-js.gt.0) then
-!            write(*,*) 'Warning: cell received > 1 levels'
-!         end if
+         if (je-js.gt.0) then
+            write(*,*) 'Warning: cell received > 1 levels'
+         end if
          do j = js, je
             k = xchIdx(j)
             if (k.le.0) then
