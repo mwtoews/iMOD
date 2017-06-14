424f1a4a93a72f7aa47c3399f994613937cdfb0d
verkaik_jo <verkaik_jo@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2017-06-14 09:12:38 +0000

    Remove testbank
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@1448 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- MODFLOW/TESTBANK/TESTRESULTS/imodflow_isg/MODFLOW_V3_00_01/run.bat
+++
@@ -1,6 +0,0 @@
-REM clean file
-IF EXIST p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\MODFLOW_V3_00_01\ERROR.TXT DEL p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\MODFLOW_V3_00_01\ERROR.TXT
-REM execute program
-p:\1206764-imod\imod30\ontwikkeling\imodflow\_exe\MODFLOW_V3_00_01_METASWAP_SVN1004_X64R.exe p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\MODFLOW_V3_00_01\imodflow.run
-REM echo exitcode
-IF ERRORLEVEL == 1 ECHO %ERRORLEVEL% > p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\MODFLOW_V3_00_01\error.txt
--- MODFLOW/TESTBANK/TESTRESULTS/imodflow_isg/imodflow_v2653/run.bat
+++
@@ -1,6 +0,0 @@
-REM clean file
-IF EXIST p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\imodflow_v2653\ERROR.TXT DEL p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\imodflow_v2653\ERROR.TXT
-REM execute program
-p:\1206764-imod\imod30\ontwikkeling\imodflow\_exe\iMODFLOW_br.exe p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\imodflow_v2653\imodflow.run
-REM echo exitcode
-IF ERRORLEVEL == 1 ECHO %ERRORLEVEL% > p:\1206764-imod\imod30\testbank\output_V3_00_01\runs\test_ALL\imodflow_isg\imodflow_v2653\error.txt
