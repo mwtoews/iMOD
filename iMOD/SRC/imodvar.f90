edfa5d5bbccebd94ea5ec52c7a7182fdd456631f
vermeul_pr <vermeul_pr@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2019-05-06 19:03:02 +0000

    start with mf6network creator
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@2472 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- iMOD/SRC/imodvar.f90
+++ iMOD/SRC/imodvar.f90
@@ -38,7 +38,7 @@ REAL(KIND=DP_KIND),PARAMETER :: PI=ATAN(1.0D0)*4.0D0  !## value pi
 LOGICAL :: LBETA=.TRUE.           !## if TRUE: Show question whether it is allowed to use Beta-version or not
 LOGICAL :: LBETA_QUESTION=.TRUE. !## overrule the question at startup
 LOGICAL :: LEXPDATE=.TRUE.        !## if TRUE: activate expire date
-INTEGER :: EXPDATE=20190515       !## expire data, after this date the iMOD-beta version cannot be used.
+INTEGER :: EXPDATE=20190615       !## expire data, after this date the iMOD-beta version cannot be used.
 INTEGER,SAVE :: ICDEBUGLEVEL      !## applied current debuglevel
 
 INTEGER,SAVE :: GKEYPRESSED    !## keypressed (cntr/shift)
@@ -49,7 +49,7 @@ CHARACTER(LEN=10),PARAMETER :: REPLACESTRING='$DBASE$'
 
 CHARACTER(LEN=30),PARAMETER :: RVERSION    ='V4_4'      !## release message - used for license
 CHARACTER(LEN=30),PARAMETER :: RVERSION_EXE='V4_4'      !## release message - only with single subnummers
-CHARACTER(LEN=50),PARAMETER :: BVERSION='Beta Build Intel v2019.1.144 [09-04-2019 20:44]'       !## banner message !!!
+CHARACTER(LEN=50),PARAMETER :: BVERSION='Beta Build Intel v2019.1.144 [06-05-2019 11:47]'       !## banner message !!!
 CHARACTER(LEN=32) :: LICFILE='I_accepted_'//TRIM(RVERSION)//'.txt'
 CHARACTER(LEN=256) :: IMFFNAME         !## name of drawing file
 CHARACTER(LEN=256) :: EXENAME,EXEPATH
