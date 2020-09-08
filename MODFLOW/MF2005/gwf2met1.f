9994cc873678e2bc8c227d0eb20390b6f0c5332c
vermeul_pr <vermeul_pr@8a8853d3-af7d-47ec-b359-4bd2096ba547>
2020-09-08 15:49:31 +0000

    minor updates glm
    git-svn-id: https://svn.oss.deltares.nl/repos/imod/trunk@3084 8a8853d3-af7d-47ec-b359-4bd2096ba547

--- MODFLOW/MF2005/gwf2met1.f
+++ MODFLOW/MF2005/gwf2met1.f
@@ -1390,11 +1390,11 @@ c create output file name
 !          if(ios.eq.0)then
 !           if(ihms.eq.0)cdate_string=time_cstring(1:8)
 !          endif
-!          write(fname,fmt) root(1:cfn_length(root)),
-!     1                       prefix(1:cfn_length(prefix)),'_',
-!     1                       cdate_string(1:cfn_length(cdate_string)),
-!     1                      '_l', ilay, partstr(1:cfn_length(partstr)),
-!     1                      '.',ext(1:cfn_length(ext))
+          write(fname,fmt) root(1:cfn_length(root)),
+     1                       prefix(1:cfn_length(prefix)),'_',
+     1                       cdate_string(1:cfn_length(cdate_string)),
+     1                      '_l', ilay, partstr(1:cfn_length(partstr)),
+     1                      '.',ext(1:cfn_length(ext))
          endif
       else ! SS
          fmt = '(5a,'//fmt
